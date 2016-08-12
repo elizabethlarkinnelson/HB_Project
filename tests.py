edafb11eff44fa915b5a2e8f1b651eec0912afad
Elizabeth Nelson <elizabeth.larkin.nelson@gmail.com>
2016-08-12 19:39:41 +0000

    tests.py initial unit test works and passed all tests.

--- tests.py
+++ tests.py
@@ -4,19 +4,27 @@ import unittest
 import server
 
 
+
 class GoalTrackerTests(unittest.TestCase):
     """Tests for goal_tracker site."""
 
     def setUp(self):
-        self.client = app.test_client()
+        self.client = server.app.test_client()
         server.app.config['TESTING'] = True
 
-    def test_hompage(self):
+    def test_homepage(self):
         result = self.client.get("/")
+        self.assertIn("New", result.data)
+
+    # def test_user_goals(self):
+    #     result = self.client.get("/user/<int:user_id>")
 
 
 
 
 
-if __name__ = '__main__':
+
+
+
+if __name__ == '__main__':
     unittest.main()
