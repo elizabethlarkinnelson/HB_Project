b11d07486ebbcf9e9a34170e5cf67f5093500181
Elizabeth Nelson <elizabeth.larkin.nelson@gmail.com>
2016-08-19 19:18:48 +0000

    Successfully added repeat_goal_inactivate_old_goal function to scheduler file.

--- scheduler.py
+++ scheduler.py
@@ -12,6 +12,28 @@ def change_goal_status():
         goal.active = False
         db.session.commit()
 
+#A function that at 00:05 will take any goal that are 
+#inactive, but exempt and create another entry of that exact goal
+#for the next week.  The previous instance of that goal
+#will then be made unexempt.
+
+def repeat_goal_inactivate_old_goal():
+    repeat_goals = Goal.query.filter((Goal.active == False) & (Goal.exempt == True)).all()
+    for goal in repeat_goals:
+        user_id = goal.user_id
+        description = goal.description
+        num_of_times = goal.num_of_times
+        new_goal = Goal(user_id=user_id, description=description,
+                        num_of_times=num_of_times)
+        db.session.add(new_goal)
+        db.session.commit()
+
+#FIX ME!!! 
+#Need to add goal schedule for when "inactivating" that current
+#weeks goals.
+#Also, need to give server access to repeat_goal_inactivate_old_goal()
+#so user can click to add the goal.
+
 
 if __name__ == "__main__":
 
